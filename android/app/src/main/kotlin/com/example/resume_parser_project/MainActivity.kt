6aa8fcc3a4282d73348fb794af9b437c01e789ee
pxooo <waleedstu@gmail.com>
2023-02-23 17:05:01 +0300

    push project

---
+++ android/app/src/main/kotlin/com/example/resume_parser_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.resume_parser_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MyApp());
+  // hi
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
+
